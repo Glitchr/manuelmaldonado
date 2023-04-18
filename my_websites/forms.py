72978e415b586caca22b19e2e80a323e9b145ebc
Manuel Maldonado <19945996+Glitchr@users.noreply.github.com>
2023-04-18 16:04:25 -0400

    Add and change animations to Index and Contact templates
    -Change index animation
    -Optimize box-shadow animations in projects template
    -Add stylization to Contact template

--- my_websites/forms.py
+++ my_websites/forms.py
@@ -2,8 +2,13 @@ from django import forms
 from hcaptcha_field import hCaptchaField
 
 
-class ContactForm(forms.Form):
+class ContactForm(forms.Form):                                                                                               
     email = forms.EmailField(required=True)
     subject = forms.CharField(required=True, max_length=200)
     message = forms.CharField(widget=forms.Textarea, required=True)
-    hcaptcha = hCaptchaField()
+    hcaptcha = hCaptchaField(label='')
+
+    hcaptcha.widget.attrs.update({'class': 'border-0 text-center form-shadow', 'style': 'padding: 0; width: 0;'})
+    email.widget.attrs.update({'class': 'form-shadow'})
+    subject.widget.attrs.update({'class': 'form-shadow'})
+    message.widget.attrs.update({'class': 'form-shadow'})
